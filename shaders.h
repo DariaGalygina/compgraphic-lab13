eca619d176dbfd8662967ffb59f86f7f9e374129
kristinash <115154097+kristinash@users.noreply.github.com>
2025-12-16 20:49:01 +0300

    Update shaders.h

--- shaders.h
+++ shaders.h
@@ -1,52 +1,42 @@
 #pragma once
+
 const char* VertexShaderSource = R"(
 #version 330 core
-in vec3 coord;
-in vec2 uv;
-out vec2 texcoord;
-flat out int index;
-uniform mat4 mvp;
-uniform vec4 offsets[10];
+layout (location = 0) in vec3 pos;
+layout (location = 1) in vec2 uv;
 
-mat4 rotateX( in float angle ) {
-	return mat4(	1.0,		0,			0,			0,
-			 		0, 	cos(angle),	-sin(angle),		0,
-					0, 	sin(angle),	 cos(angle),		0,
-					0, 			0,			  0, 		1);
-}
+out vec2 texCoord;
+flat out int instanceID;
 
-mat4 rotateY( in float angle ) {
-	return mat4(	cos(angle),		0,		sin(angle),	0,
-			 				0,		1.0,			 0,	0,
-					-sin(angle),	0,		cos(angle),	0,
-							0, 		0,				0,	1);
-}
+uniform mat4 view;
+uniform mat4 proj;
+uniform vec4 planetData[10]; // x: dist, y: scale, z: selfRot, w: orbitRot
 
-mat4 rotateZ( in float angle ) {
-	return mat4(	cos(angle),		-sin(angle),	0,	0,
-			 		sin(angle),		cos(angle),		0,	0,
-							0,				0,		1,	0,
-							0,				0,		0,	1);
+mat4 rotateY(float angle) {
+    return mat4(cos(angle), 0, sin(angle), 0, 0, 1, 0, 0, -sin(angle), 0, cos(angle), 0, 0, 0, 0, 1);
 }
 
 void main() {
-	float offset = offsets[gl_InstanceID].x;
-    float scale = offsets[gl_InstanceID].y;
-	float rot_axis = offsets[gl_InstanceID].z;
-	float rot_sun = offsets[gl_InstanceID].w;
-	vec4 pos = rotateY(rot_axis) * vec4(coord * scale * 2, 1.0);
-    pos = rotateY(rot_sun) * (pos + vec4(offset, 0.0, 0.0, 0.0));
-    gl_Position = mvp * pos;
-	texcoord = uv;
-	index = gl_InstanceID;
+    vec4 data = planetData[gl_InstanceID];
+    
+    // 1. Поворот вокруг своей оси и масштаб
+    vec4 localPos = rotateY(data.z) * vec4(pos * data.y, 1.0);
+    // 2. Смещение на радиус орбиты
+    vec4 worldPos = localPos + vec4(data.x, 0, 0, 0);
+    // 3. Вращение по орбите вокруг Солнца
+    vec4 finalPos = rotateY(data.w) * worldPos;
+
+    gl_Position = proj * view * finalPos;
+    texCoord = uv;
+    instanceID = gl_InstanceID;
 })";
 
 const char* FragShaderSource = R"(
 #version 330 core
-in vec2 texcoord;
-flat in int index;
-uniform sampler2D tex[10];
+in vec2 texCoord;
+flat in int instanceID;
+uniform sampler2D texSamplers[10];
 
 void main() {
-	gl_FragColor = texture(tex[index], texcoord);
+    gl_FragColor = texture(texSamplers[instanceID], texCoord);
 })";
